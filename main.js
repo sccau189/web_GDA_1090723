e7fcd3f395088b595cb28f39dcf91f0e98af2f81
a123f456c789 <66512956+a123f456c789@users.noreply.github.com>
2020-07-24 20:46:47 +0800

    1090724

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("js")
+$(document).ready(function(){
+    $('#about2 #about1').hover(function(){
+        $('#about2').fadeIn(1000),function(){
+            $('about2. about3').fadeOut(1000)
+        }
+    })
+})
