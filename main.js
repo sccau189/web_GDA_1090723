3cb2930b57af33c870eecb5e716ca5ff87449bcd
sccau189 <sccau189@hotmail.com>
2020-07-26 20:30:43 +0800

    1090726

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log("js")
-$(document).ready(function(){
-    $('#about2 #about1').hover(function(){
-        $('#about2').fadeIn(1000),function(){
-            $('about2. about3').fadeOut(1000)
-        }
-    })
-})
